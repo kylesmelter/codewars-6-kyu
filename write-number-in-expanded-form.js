699cb31a4a13b2025547f002aeca1dc4f8dd1d11
Kyle Smelter <kylesmelter@gmail.com>
2023-01-27 16:18:22 -0800

    write-number-in-expanded-form

---
+++ write-number-in-expanded-form.js
@@ -0,0 +1,22 @@
+// Write Number in Expanded Form
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+// NOTE: All numbers will be whole numbers greater than 0.
+
+// If you liked this kata, check out part 2!!
+
+function expandedForm(num) {
+	num = String(num);
+	let result = [];
+	for (var i = 0; i < num.length; i++) {
+	    if (num[i] === '0') {
+            continue;
+        } else {
+            result.push(num[i] + "0".repeat(num.length -i -1))
+        };
+	};
+	return result.join(" + ");
+};
